91c2bed1ed10799c04cb9c868c95a7854d6e20e6
Ashton <30474771+ScripteJunkie@users.noreply.github.com>
2021-04-11 19:36:03 -0400

    Fixed React App

--- test/motrou/src/App.js
+++ test/motrou/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {Editor, EditorState} from 'draft-js'
 import './App.css';
+import {getDefaultKeyBinding, KeyBindingUtil, RichUtils, convertToRaw, convertFromRaw} from 'draft-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  
+    const content = window.localStorage.getItem('content');
+  
+    if (content) {
+      this.state.editorState = EditorState.createWithContent(convertFromRaw(JSON.parse(content)));
+    } else {
+      this.state.editorState = EditorState.createEmpty();
+    }
+  }
+  
+  onChange = editorState => {const contentState = editorState.getCurrentContent(); this.saveContent(contentState); this.setState({editorState})};
+
+  keyBindingFn = (event) => {
+    if (KeyBindingUtil.hasCommandModifier(event) && event.keyCode === 75) {event.preventDefault(); return 'bbbold';}
+    if (event.keyCode === 9) {event.preventDefault(); return 'tab';}
+    return getDefaultKeyBinding(event);
+  }
+
+  handleKeyCommand = (command) => {
+    let newState;
+    if (command === 'bbbold') {
+      newState = RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD');
+    }
+
+    if (command === 'tab') {
+      newState = RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD');
+    }
+
+    if (newState) {
+      this.setState({ editorState: newState });
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+
+  saveContent = (content) => {
+    window.localStorage.setItem('content', JSON.stringify(convertToRaw(content)));
+  }
+
+  render() {
+    return (
+      <div className="editor">
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+          handleKeyCommand={this.handleKeyCommand}
+          keyBindingFn={this.KeyBindingFn}
+          />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ test/motrou/src/Swap.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './App.css';
+let root = document.documentElement;
+var themeColor = 'light';
+
+export default class Swap extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { };
+      
+        const theme = window.localStorage.getItem('theme');
+        if (theme === "dark") {
+            root.style.setProperty("--color-primary", 'black');
+            root.style.setProperty("--color-font", 'white');
+            themeColor = 'dark';
+        } else {
+            root.style.setProperty("--color-primary", 'white');
+            root.style.setProperty("--color-font", 'black');
+            themeColor = 'light';
+        }
+      }
+    handleClick = () => {
+        // light or dark theme
+        if (themeColor === 'dark') {
+            root.style.setProperty("--color-primary", 'white');
+            root.style.setProperty("--color-font", 'black');
+            this.saveTheme('light');
+        }
+        else {
+            root.style.setProperty("--color-primary", 'black');
+            root.style.setProperty("--color-font", 'white');
+            this.saveTheme('dark');
+        }
+    };
+
+    saveTheme = (theme) => {
+        window.localStorage.setItem('theme', theme);
+        themeColor = theme;
+      }
+
+  render() {
+    return (
+      <div className="swap" onClick={this.handleClick}>
+      </div>
+    );
+  }
+}
