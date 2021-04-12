2670da40348e77d9ef085ce6cda086979cb9b7c3
Ashton <30474771+ScripteJunkie@users.noreply.github.com>
2021-04-11 20:40:11 -0400

    Formatting prototype

---
+++ test/motrou/src/Split.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class Split extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+  render() {
+    return (
+      <div className="split">
+      </div>
+    );
+  }
+}
